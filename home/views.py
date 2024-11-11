44f5060da23a787d60bd478c2a614b0125a37f64
mezter1337 <andreas.javert@gmail.com>
2024-11-11 12:48:04 +0100

    Finish with description in detail view and seeing recipes in the homepage

--- home/views.py
+++ home/views.py
@@ -1,8 +1,13 @@
 from django.views.generic import ListView
 from recipes.models import Recipe
 
-
 class Index(ListView):
     template_name = "home/index.html"
     model = Recipe
-    context_object_name = "recipes"
+    context_object_name = 'recipes'
+    
+    def get_queryset(self):
+        return self.model.objects.all()[:3]
+    
+    
+    
